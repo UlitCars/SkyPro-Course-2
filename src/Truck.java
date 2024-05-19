7432f46e0818c7e86d229b58514b7ba6c6ae941a
UlitCars <zashibisf@gmail.com>
2024-05-19 16:34:10 +0300

    Course 2 Lesson 3.0

---
+++ src/Truck.java
@@ -0,0 +1,13 @@
+public class Track extends TrackAndCar {
+
+    public Track(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    public void checkTrailer() {
+
+
+
+            System.out.println("Проверяем прицеп");
+        }
+}
